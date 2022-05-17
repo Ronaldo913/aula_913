e92474d3780b058e4adaf0c760d7c8ca8b9a53de
Tarsis <tarsis.souza@ifal.edu.br>
2022-05-17 08:02:56 -0300

    Aula 07 - Interfaces Gráficas - 16/05
    Nesta aula foram abordados elementos de interface gráfica por meio do  desenvolvimento de telas semelhantes ao aplicativo Hotel Urbano.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,10 +11,22 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-              margin: const EdgeInsets.all(16),
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        centerTitle: false,
+        backgroundColor: Color(0xFF10397B),
+        title: Text(
+          'Pesquisar',
+          style: TextStyle(
+            fontSize: 24
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Container(
               padding: const EdgeInsets.all(16),
               color: Colors.purple,
               child: Row(
@@ -62,8 +74,88 @@ class _HomePageState extends State<HomePage> {
                   )
                 ],
               ),
-          )
-        ],
+            ),
+            const SizedBox(height: 16),
+            Card(
+              child: Column(
+                children: [
+                  const Placeholder(fallbackHeight: 150),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Pacote Cancún 2021',
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'Aéreo - Hotel All incluse',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.wb_sunny_outlined,
+                              color: Colors.grey[700],
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              '05 Diárias',
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                            SizedBox(width: 8),
+                            Icon(
+                              Icons.person_outline,
+                              color: Colors.grey[700],
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              '01 Pessoa',
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'A partir de R\$ 6816',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        Row(
+                          children: [
+                            const Text(
+                              'R\$ 3.749',
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFFFD6C00),
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              'Taxa Grátis em até 12x',
+                              style: TextStyle(color: Colors.grey[700]),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          'Cancelamento Grátis!',
+                          style: TextStyle(color: Colors.green[800]),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
