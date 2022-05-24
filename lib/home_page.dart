55e7f2d410377747c33f83ed3388e87b4f3d5f37
Tarsis <tarsis.souza@ifal.edu.br>
2022-05-24 08:08:40 -0300

    Aula 08 - Interfaces Gráficas - 24/05
    Nesta aula foram abordados elementos de interface gráfica por meio do  desenvolvimento de telas semelhantes ao aplicativo Hotel

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,15 +11,13 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
+      backgroundColor: Colors.grey,
       appBar: AppBar(
         centerTitle: false,
         backgroundColor: Color(0xFF10397B),
-        title: Text(
+        title: const Text(
           'Pesquisar',
-          style: TextStyle(
-            fontSize: 24
-          ),
+          style: TextStyle(fontSize: 24),
         ),
       ),
       body: Padding(
@@ -27,8 +25,11 @@ class _HomePageState extends State<HomePage> {
         child: ListView(
           children: [
             Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.purple,
+              ),
               padding: const EdgeInsets.all(16),
-              color: Colors.purple,
               child: Row(
                 children: [
                   Expanded(
@@ -76,86 +77,124 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             const SizedBox(height: 16),
-            Card(
-              child: Column(
-                children: [
-                  const Placeholder(fallbackHeight: 150),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Pacote Cancún 2021',
-                          style: TextStyle(
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          'Aéreo - Hotel All incluse',
-                          style: TextStyle(color: Colors.grey[700]),
-                        ),
-                        const SizedBox(height: 8),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.wb_sunny_outlined,
-                              color: Colors.grey[700],
-                            ),
-                            SizedBox(width: 4),
-                            Text(
-                              '05 Diárias',
-                              style: TextStyle(color: Colors.grey[700]),
-                            ),
-                            SizedBox(width: 8),
-                            Icon(
-                              Icons.person_outline,
-                              color: Colors.grey[700],
-                            ),
-                            SizedBox(width: 4),
-                            Text(
-                              '01 Pessoa',
-                              style: TextStyle(color: Colors.grey[700]),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          'A partir de R\$ 6816',
-                          style: TextStyle(color: Colors.grey[700]),
-                        ),
-                        Row(
-                          children: [
-                            const Text(
-                              'R\$ 3.749',
-                              style: TextStyle(
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFFFD6C00),
-                              ),
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              'Taxa Grátis em até 12x',
-                              style: TextStyle(color: Colors.grey[700]),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          'Cancelamento Grátis!',
-                          style: TextStyle(color: Colors.green[800]),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+            buildCardPacoteTuristico(
+              imagem: 'https://r4t2s9v3.stackpathcdn.com/wp-content/uploads/2018/01/beleza-cancun.jpg',
+              titulo: 'Pacote Cancún 2023',
+              transporte: 'Aéreo - Hotel all inclusve',
+              numDiarias: 5,
+              numPessoas: 2,
+              precoAntigo: 6819,
+              precoAtual: 2819,
+              numParcelas: 12
+            ),
+            buildCardPacoteTuristico(
+                imagem: 'https://www.viagensecaminhos.com/wp-content/uploads/2014/10/maragogi-alagoas.jpg',
+                titulo: 'Pacote Maragogi 2023',
+                transporte: 'Hotel all inclusve',
+                numDiarias: 7,
+                numPessoas: 1,
+                precoAntigo: 4819,
+                precoAtual: 1500,
+                numParcelas: 6
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  buildCardPacoteTuristico({
+    required String imagem,
+    required String titulo,
+    required String transporte,
+    required int numDiarias,
+    required int numPessoas,
+    required double precoAntigo,
+    required double precoAtual,
+    required int numParcelas,
+  }) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(8)),
+            child: Image.network(imagem),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  titulo,
+                  style: const TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  transporte,
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.wb_sunny_outlined,
+                      color: Colors.grey[700],
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      '$numDiarias Diárias',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                    const SizedBox(width: 8),
+                    Icon(
+                      Icons.person_outline,
+                      color: Colors.grey[700],
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      '$numPessoas Pessoas',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'A partir de R\$ $precoAntigo',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'R\$ $precoAtual',
+                      style: const TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFFD6C00),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      'Taxa Grátis em até ${numParcelas}x',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Cancelamento Grátis!',
+                  style: TextStyle(color: Colors.green[800]),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
