03de35c05f76b9356e8a8a1d2fe3f6a927e86740
Ronaldo Noberto <rns6@aluno.ifal.edu.br>
2022-06-12 14:56:21 -0300

    algumas coisas a mais

---
+++ lib/pages/pacote_page.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class PacotePage extends StatefulWidget {
+  const PacotePage({Key? key}) : super(key: key);
+
+  @override
+  _PacotePageState createState() => _PacotePageState();
+}
+
+class _PacotePageState extends State<PacotePage> {
+  
+}
