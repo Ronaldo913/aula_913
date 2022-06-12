11cbe54ce8079ae1b0274a4a316ea9c1dc5eb82a
Ronaldo Noberto <rns6@aluno.ifal.edu.br>
2022-06-12 15:07:17 -0300

    tudo ok

--- lib/pages/pacote_page.dart
+++ lib/pages/pacote_page.dart
@@ -9,5 +9,135 @@ class PacotePage extends StatefulWidget {
 }
 
 class _PacotePageState extends State<PacotePage> {
-  
+  PacoteEstudo pacote1 = PacoteEstudo(
+    imagem:
+        'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
+    titulo: 'Pacote Cancún 2022/2023',
+    transporte: 'Aéreo - Hotel All inclusive',
+    cidade: 'CANCÚN, MEX',
+    validade: 'De 10 ago 2021 até 30 set 2022',
+    desconto: 45,
+    numDiarias: 5,
+    numPessoas: 2,
+    numParcelas: 6,
+    precoAntigo: 6819,
+    precoAtual: 2819,
+  );
+  PacoteEstudo pacote2 = PacoteEstudo(
+    imagem: 'https://letsdive.com.br/wp-content/uploads/2019/04/PROCURANDO-MERGULHO-EM-MARAGOGI-CONSIDERE-MACEIO.png',
+    titulo: 'Pacote Maragogi 2023',
+    transporte: 'Hotel All inclusive',
+    cidade: 'MARAGOGI, BRA',
+    validade: 'De 10 jan 2022 até 31 dez 2023',
+    desconto: 45,
+    numDiarias: 7,
+    numPessoas: 3,
+    numParcelas: 12,
+    precoAntigo: 4819,
+    precoAtual: 819,
+  );
+
+  PacoteEstudo pacote3 = PacoteEstudo(
+    imagem: 'https://letsdive.com.br/wp-content/uploads/2019/04/PROCURANDO-MERGULHO-EM-MARAGOGI-CONSIDERE-MACEIO.png',
+    titulo: 'Pacote Maragogi 2023',
+    transporte: 'Hotel All inclusive',
+    cidade: 'MARAGOGI, BRA',
+    validade: 'De 10 jan 2022 até 31 dez 2023',
+    desconto: 45,
+    numDiarias: 7,
+    numPessoas: 3,
+    numParcelas: 12,
+    precoAntigo: 4819,
+    precoAtual: 819,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        backgroundColor: const Color(0xFF10397B),
+        title: const Text(
+          'Pesquisar',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+      backgroundColor: Colors.grey[100],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const Text(
+                          'Tops destinos mais buscados',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Text(
+                          'Corre que tá rolando muita promoção',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: onPressedButton,
+                          child: const Text(
+                            'EU QUERO!',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.black,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xFFF8FF04),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 24),
+                  Placeholder(
+                    fallbackHeight: 150,
+                    fallbackWidth: 100,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            CardPacoteTuristico(pacoteEstudo: pacote1),
+            CardPacoteTuristico(pacoteEstudo: pacote2),
+            CardPacoteTuristico(pacoteEstudo: pacote3),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onPressedButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return const PropagandaPage();
+        },
+      ),
+    );
+  }
 }
